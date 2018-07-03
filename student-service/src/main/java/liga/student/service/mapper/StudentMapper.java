dab95e4ac82f0bcb25104cfdeb8f6955b17fad49
liga12 <Ligalauz12>
2018-07-03 09:53:45 +0300

    add tests studentMapper

--- student-service/src/main/java/liga/student/service/mapper/StudentMapper.java
+++ student-service/src/main/java/liga/student/service/mapper/StudentMapper.java
@@ -2,16 +2,9 @@ package liga.student.service.mapper;
 
 import liga.student.service.domain.Student;
 import liga.student.service.dto.StudentDto;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.mapstruct.Mapper;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface StudentMapper {
