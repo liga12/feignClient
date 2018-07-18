5d91da5b3980369c1d914c50d76eb6f75832f57f
liga12 <Ligalauz12>
2018-07-18 18:02:48 +0300

    integration test

--- student-service/src/test/java/liga/student/service/controller/StudentMainControllerIntegrationTest.java
+++ student-service/src/test/java/liga/student/service/controller/StudentMainControllerIntegrationTest.java
@@ -1,13 +1,12 @@
 package liga.student.service.controller;
 
-import static org.assertj.core.api.BDDAssertions.*;
-import static org.junit.Assert.assertEquals;
-
 import liga.student.service.StudentClientService;
+import liga.student.service.domain.StudentRepository;
 import liga.student.service.dto.StudentDTO;
 import liga.student.service.service.MongoConfig;
 import liga.student.service.service.StudentService;
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,10 +19,20 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.net.URI;
+import java.util.*;
+
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {StudentClientService.class, MongoConfig.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -31,8 +40,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class StudentMainControllerIntegrationTest {
 
     @Autowired
+    private StudentRepository studentRepository;
+
+    @Autowired
     private StudentService studentService;
-    static ConfigurableApplicationContext eurekaServer;
+    private static ConfigurableApplicationContext eurekaServer;
 
     @BeforeClass
     public static void startEureka() {
@@ -52,19 +64,121 @@ public class StudentMainControllerIntegrationTest {
 
     @Autowired
     private TestRestTemplate testRestTemplate;
+    private ParameterizedTypeReference<List<StudentDTO>> studentDTOList = new ParameterizedTypeReference<List<StudentDTO>>() {
+    };
+
+    @Before
+    public void setUp() {
+        studentRepository.deleteAll();
+    }
 
     @Test
-    public void shouldRegisterClientInEurekaServer() throws InterruptedException {
-        StudentDTO studentDTO = studentService.create(StudentDTO.builder().name("name").surname("surname").age(25).build());
-        // registration has to take place...
-        Thread.sleep(3000);
-        System.out.println(port);
-
-        ResponseEntity<String> response = this.testRestTemplate.getForEntity("http://localhost:" + port + "/student/", String.class);
-
+    public void getStudents() {
+        StudentDTO first = studentService.create(StudentDTO.builder().name("n").surname("s").age(20).build());
+        StudentDTO second = studentService.create(StudentDTO.builder().id("2").name("n2").surname("s2").age(22).build());
+        ResponseEntity<List<StudentDTO>> response = testRestTemplate.exchange(getURL(), HttpMethod.GET, null, studentDTOList);
         then(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertEquals(response.getBody().toString(),StudentDTO.builder().name("name").surname("surname").age(25).build().toString());
-//        then(response.getBody()).contains("a-bootiful-client");
+        assertEquals(response.getBody(), Arrays.asList(first, second));
+    }
+
+    @Test
+    public void getStudentById() {
+        StudentDTO first = studentService.create(StudentDTO.builder().name("n").surname("s").age(20).build());
+        ResponseEntity<StudentDTO> response = testRestTemplate.exchange(getURL() + "id/" + first.getId(), HttpMethod.GET, null, StudentDTO.class);
+        then(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertEquals(response.getBody(), first);
+        response = testRestTemplate.exchange(getURL() + "id/2", HttpMethod.GET, null, StudentDTO.class);
+        then(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertNull(response.getBody());
+    }
+
+    @Test
+    public void getStudentByName() {
+        StudentDTO first = studentService.create(StudentDTO.builder().name("n").surname("s").age(20).build());
+        ResponseEntity<List<StudentDTO>> response = testRestTemplate
+                .exchange(getURL() + "name/" + first.getName(), HttpMethod.GET, null, studentDTOList);
+        then(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertEquals(response.getBody(), Collections.singletonList(first));
+        response = testRestTemplate.exchange(getURL() + "name/n3", HttpMethod.GET, null, studentDTOList);
+        then(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertEquals(response.getBody(), new ArrayList<>());
+    }
+
+    @Test
+    public void getStudentBySurname() {
+        StudentDTO first = studentService.create(StudentDTO.builder().name("n").surname("s").age(20).build());
+        ResponseEntity<List<StudentDTO>> response = testRestTemplate
+                .exchange(getURL() + "surname/" + first.getSurname(), HttpMethod.GET, null, studentDTOList);
+        then(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertEquals(response.getBody(), Collections.singletonList(first));
+        response = testRestTemplate.exchange(getURL() + "surname/s3", HttpMethod.GET, null, studentDTOList);
+        then(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertEquals(response.getBody(), new ArrayList<>());
+    }
+
+    @Test
+    public void getStudentByAge() {
+        StudentDTO first = studentService.create(StudentDTO.builder().name("n").surname("s").age(20).build());
+        ResponseEntity<List<StudentDTO>> response = testRestTemplate
+                .exchange(getURL() + "age/" + first.getAge(), HttpMethod.GET, null, studentDTOList);
+        then(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertEquals(response.getBody(), Collections.singletonList(first));
+        response = testRestTemplate.exchange(getURL() + "age/21", HttpMethod.GET, null, studentDTOList);
+        then(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertEquals(response.getBody(), new ArrayList<>());
+    }
+
+    @Test
+    public void createStudent() throws Exception {
+        StudentDTO first = StudentDTO.builder().name("n").surname("s").age(20).build();
+        ResponseEntity<StudentDTO> response = testRestTemplate
+                .exchange(RequestEntity.put(new URI(getURL())).body(first), StudentDTO.class);
+        ResponseEntity<StudentDTO> response2 = testRestTemplate.
+                exchange(getURL() + "id/" + Objects
+                        .requireNonNull(response.getBody()).getId(), HttpMethod.GET, null, StudentDTO.class);
+        then(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertEquals(response.getBody(), response2.getBody());
+        ResponseEntity<List<StudentDTO>> response3 = testRestTemplate.exchange(getURL(), HttpMethod.GET, null, studentDTOList);
+        then(response3.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertEquals(Objects.requireNonNull(response3.getBody()).size(),  1);
+    }
+
+    @Test
+    public void updateStudent() throws Exception {
+        StudentDTO first = studentService.create(StudentDTO.builder().name("n").surname("s").age(20).build());
+        first.setName("n2");
+        ResponseEntity<StudentDTO> response = testRestTemplate
+                .exchange(RequestEntity.post(new URI(getURL())).body(first), StudentDTO.class);
+        ResponseEntity<List<StudentDTO>> response2 = testRestTemplate
+                .exchange(getURL() + "name/" + first.getName(), HttpMethod.GET, null, studentDTOList);
+        then(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertEquals(response.getBody(), Objects.requireNonNull(response2.getBody()).get(0));
+         response2 = testRestTemplate
+                .exchange(getURL() + "name/n" + first.getName(), HttpMethod.GET, null, studentDTOList);
+        then(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertEquals(response2.getBody(), new ArrayList<>());
+        ResponseEntity<List<StudentDTO>> response3 = testRestTemplate.exchange(getURL(), HttpMethod.GET, null, studentDTOList);
+        then(response3.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertEquals(Objects.requireNonNull(response3.getBody()).size(),  1);
+    }
+
+    @Test
+    public void deleteStudent(){
+        StudentDTO first = studentService.create(StudentDTO.builder().name("n").surname("s").age(20).build());
+        ResponseEntity<StudentDTO> response = testRestTemplate
+                .exchange(getURL() + "/"+first.getId(), HttpMethod.DELETE, null, StudentDTO.class);
+        then(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        ResponseEntity<StudentDTO> response2 = testRestTemplate.exchange(getURL() + "id/" + first.getId(), HttpMethod.GET, null, StudentDTO.class);
+        then(response2.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertNull(response2.getBody());
+        ResponseEntity<List<StudentDTO>> response3 = testRestTemplate.exchange(getURL(), HttpMethod.GET, null, studentDTOList);
+        then(response3.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertEquals(response3.getBody(),  new ArrayList<>());
+
+    }
+
+    private String getURL() {
+        return "http://localhost:" + port + "/student/";
     }
 
 
--- student-service/src/test/java/liga/student/service/controller/StudentRelationControllerTest.java
+++ student-service/src/test/java/liga/student/service/controller/StudentRelationControllerTest.java
@@ -1,14 +1,11 @@
 package liga.student.service.controller;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import liga.student.service.dto.StudentDTO;
 import liga.student.service.service.StudentService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -17,6 +14,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
